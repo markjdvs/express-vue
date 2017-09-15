7251f10572f4920b96cbd7e4081894b4ce034265
mrkjdvs <mjd814@gmail.com>
2017-09-15 12:06:05 +0100

    New event component created. AddEvent declared and tested to clear inputs on click and pushed into array of events.

--- vue-frontend/src/components/EventsNew.vue
+++ vue-frontend/src/components/EventsNew.vue
@@ -3,19 +3,26 @@
     <h1>Add an Event</h1>
     <input
       type="text"
-      name=""
-      v-model=""
-      value="">
+      v-model="event.title"
+      placeholder="Event Title" />
+    <input
+      type="date"
+      v-model="event.dateFrom"
+      placeholder="When does the event start?" />
+    <input
+      type="date"
+      v-model="event.dateTo"
+      placeholder="When does the event end?" />
     <input
       type="text"
-      name=""
-      v-model=""
-      value="">
-    <input
+      v-model="event.location"
+      placeholder="Event Location" />
+    <textarea
       type="text"
-      name=""
-      v-model=""
-      value="">
+      v-model="event.description"
+      placeholder="Event Description">
+    </textarea>
+    <button @click="addEvent">Create</button>
   </div>
 </template>
 
@@ -23,6 +30,26 @@
   export default {
     data () {
       return {
+        event: {
+          title: '',
+          dateFrom: '',
+          dateTo: '',
+          location: '',
+          description: ''
+        },
+        events: []
+      }
+    },
+    methods: {
+      addEvent: function() {
+        this.events.push(this.event);
+        this.event = {
+          title: '',
+          dateFrom: '',
+          dateTo: '',
+          location: '',
+          description: ''
+        };
       }
     }
   }
