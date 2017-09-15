98f6a4e7edbe196508da1dad7cd497f3e4e612f1
mrkjdvs <mjd814@gmail.com>
2017-09-15 10:32:11 +0100

    Installed morgan. Added environment file and checked server still working and connected up.

--- express-backend/server.js
+++ express-backend/server.js
@@ -1,9 +1,15 @@
 const express = require('express');
+const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const cors = require('cors');
+const { port, env, dbURI } = require('./config/environment');
 const routes = require('./config/routes');
+
 const app = express();
+
+if(env !== 'test') app.use(morgan('dev'));
 
 app.use(routes);
 
-app.listen(8000, () => console.log(`Express is listening on port 8000`));
+app.listen(port, () => console.log(`Express is listening on port ${port}`));
