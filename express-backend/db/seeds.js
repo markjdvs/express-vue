31f4edf6043952671a026c6e4ea620c65c07cf52
mrkjdvs <mjd814@gmail.com>
2017-09-16 10:18:17 +0100

    2 more entries for seeds file added. 3 events total now.

--- express-backend/db/seeds.js
+++ express-backend/db/seeds.js
@@ -14,8 +14,22 @@ Event
     title: 'Glastonbury Festival',
     dateFrom: moment('2017-06-21'),
     dateTo: moment('2017-06-26'),
-    location: 'Glastonbury',
-    description: 'A weekend to remember!'
+    location: 'Glastonbury, UK',
+    description: 'A festival to remember!'
+  },
+  {
+    title: 'Christmas Day',
+    dateFrom: moment('2017-12-25'),
+    dateTo: moment('2017-12-25'),
+    location: '10 Downing Street, London, UK',
+    description: `Everyone's welcome!`
+  },
+  {
+    title: 'Hopefully a better year for the world!',
+    dateFrom: moment('2018-01-01'),
+    dateTo: moment('2018-12-31'),
+    location: 'Various locations around the globe.',
+    description: `Let's all just try to get along, hey?`
   }])
   .then((events) => {
     console.log(`${events.length} events created.`);
